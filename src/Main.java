8a2683d7be627d323d49c368b578dd6a2b28a488
Zlata <rixter.ar@gmail.com>
2024-11-25 00:59:07 +0900

    Добавлен метод calc

--- src/Main.java
+++ src/Main.java
@@ -7,42 +7,53 @@ class Main {
 
         try {
             String input = scanner.nextLine();
-            String[] parts = input.split(" ");
-
-            if (parts.length != 3) {
-                throw new Exception("Неверный формат");
-            }
-
-            int a = Integer.parseInt(parts[0]);
-            int b = Integer.parseInt(parts[2]);
-            String operator = parts[1];
-
-            if (a < 1 || a > 10 || b < 1 || b > 10) {
-                throw new Exception("Число вне диапазона");
-            }
-
-            int result;
-            switch (operator) {
-                case "+":
-                    result = a + b;
-                    break;
-                case "-":
-                    result = a - b;
-                    break;
-                case "*":
-                    result = a * b;
-                    break;
-                case "/":
-                    if (b == 0) throw new Exception("Деление на ноль");
-                    result = a / b;
-                    break;
-                default:
-                    throw new Exception("Неверный оператор");
-            }
-
+            String result = calc(input);
             System.out.println("Ответ: " + result);
-        } catch (Exception e) {
-            System.out.println("Ошибка: throws Exception");
         }
+        catch (NumberFormatException e) {
+            System.out.println("Ошибка: Введите числа в корректном формате (например, 1 + 2)");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Ошибка: Деление на ноль");
+        }
+
+        catch (Exception e) {
+            System.out.println("Ошибка");
+        }
+    }
+
+    public static String calc(String input) throws Exception {
+        String[] parts = input.split(" ");
+
+        if (parts.length != 3) {
+            throw new Exception("Ошибка: Неверный ввод.");
+        }
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[2]);
+        String operator = parts[1];
+
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            throw new Exception("Числа должны быть от 1 до 10 включительно");}
+        {
+
+        }
+        int result;
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                throw new Exception("Неверный оператор");
+        }
+        return String.valueOf(result);
     }
 }
